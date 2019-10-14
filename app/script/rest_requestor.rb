2bae6ea4cafc6f1ede2fcda022efbf5ee0ec0118
jucora <artjulius@hotmail.com>
2019-10-14 14:42:27 -0500

    exercise completed

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,4 +1,12 @@
 require 'rest-client'
 url = "http://localhost:3000/users"
+url2 = "http://localhost:3000/users/new"
+url3 = "http://localhost:3000/users/1/edit"
+url4 = "http://localhost:3000/users/show"
 
 puts RestClient.get(url)
+puts RestClient.get(url2)
+puts RestClient.get(url3)
+puts RestClient.get(url4)
+
+puts RestClient.post(url, "")
